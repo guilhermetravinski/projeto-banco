87cc8104f32db68a7abc32471d68c589e46ad584
guilhermeTravinski <guitrafer@gmail.com>
2022-01-10 20:47:06 -0300

    Create account

--- src/index.js
+++ src/index.js
@@ -1,5 +1,33 @@
 const express = require('express');
+const { v4: uuidv4 } = require("uuid");
 
 const app = express();
+
+app.use(express.json());
+
+const custumers = [];
+
+/**
+ * cpf - string
+ * name - string
+ * id - uuid (universally unique identifier)
+ * statement []
+ */
+
+app.post("/account", (request, response) => {
+    const { cpf, name } = request.body;
+    
+    const id = uuidv4();
+
+    custumers.push({
+        cpf,
+        name,
+        id,
+        statement: []
+    });
+
+    return response.status(201).send();
+});
+
 
 app.listen(3333);
