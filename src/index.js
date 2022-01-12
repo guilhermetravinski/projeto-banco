d2c9b1b41b8efb03e7356cde9d92b02bc1046c2e
guilhermeTravinski <guitrafer@gmail.com>
2022-01-12 20:35:17 -0300

    Final

--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 const { request, response } = require('express');
 const express = require('express');
+const { get } = require('express/lib/response');
 const res = require('express/lib/response');
 const { v4: uuidv4 } = require("uuid");
 
@@ -114,5 +115,36 @@ app.get("/statement/date", verifyIfExistsAccountCPF, (request, response) => {
 
     return response.json(statement);
 });
+
+app.put("/account", verifyIfExistsAccountCPF, (request, response) => {
+    const { name } = request.body;
+    const { customer } = request;
+
+    customer.name = name;
+
+    return response.status(201).send();
+});
+
+app.get("/account", verifyIfExistsAccountCPF, (request, response) => {
+    const { customer } = request;
+
+    return response.json(customer);
+})
+
+app.delete("/account", verifyIfExistsAccountCPF, (request, response) => {
+    const { customer } = request;
+
+    customers.splice(customer, 1);
+
+    return response.status(200).json(customers);
+});
+
+app.get("/balance", verifyIfExistsAccountCPF, (request, response) => {
+    const { customer } = request;
+
+    const balance = getBalance(customer.statement);
+
+    return response.json(balance);
+})
 
 app.listen(3333);
